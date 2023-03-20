c13e145b36c304864a8e7f7341ad7e5189dcda9c
tycrek <gpg+github@jmoore.dev>
2023-03-19 23:01:50 -0600

    feat: added McSkin command

--- src/commands.ts
+++ src/commands.ts
@@ -8,8 +8,28 @@ const HelloCommand: Command = {
 		const user = interaction.member.user.id;
 		return Promise.resolve(`Hello <@${user}>!`);
 	}
-}
+};
+
+const McSkinCommand: Command = {
+	name: 'mcskin',
+	description: 'Gets the skin of a Minecraft player',
+	options: [
+		{
+			name: 'player',
+			description: 'The Minecraft player to get the skin of',
+			type: ApplicationCommandOptionType.STRING,
+			autocomplete: true,
+			required: true,
+			max_length: 16,
+		}
+	],
+	run: (interaction) => {
+		const player = interaction.data.options[0].value;
+		return Promise.resolve(`https://mulv.tycrek.dev/api/lookup?username=${player}&type=skin`);
+	}
+};
 
 export const Commands = [
-	HelloCommand
+	HelloCommand,
+	McSkinCommand,
 ];
